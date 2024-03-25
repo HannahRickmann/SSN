a72ea791c58782ca434dc6ad94b1e78c3f3e1738
Jakob-L-M <rickmann99@web.de>
2024-03-25 11:31:56 +0100

    Found two cycle types for n = 3, upper bound, put that in custom experiments

--- main.py
+++ main.py
@@ -18,25 +18,6 @@ def try_all_possibilities():
         experiment.choose_experiment(id)
         experiment.run(5)
 
-def try_previous_random_experiment(time):
-    n = 3
-
-    file_list = os.listdir(f'./experiments/results/{time}/')
-    file_list = [file_name for file_name in file_list if f'random_experiment_dim_{n}' in file_name]
-
-    numbers = []
-    for file_name in file_list:
-        start_index = file_name.find('nr_') + 3
-        end_index = file_name.find('.json')
-        numbers.append(file_name[start_index:end_index])
-
-    for number in numbers:
-        experiment = Experiment(number)
-        experiment.read_custom_data('random_experiment_dim_3', folder=f'results/{time}')
-        experiment.run(5, save = False)
-        print(f'experiment number: {number}')
-        experiment.analyse_active_set_cycle()
-
 def try_multiplying_constant(exp_nr, c):
     experiment = Experiment(exp_nr)
     experiment.read_custom_data('custom_experiment')
@@ -70,13 +51,30 @@ def try_random_experiment(amount, n, upper_bound = False):
         experiment = Experiment(i)
         experiment.current_time = current_time
         experiment.generate_data(n, upper_bound)
-        experiment.run(10)
+        experiment.run(10, save=True)
+
+def try_previous_random_experiment(time):
+    n = 3
+
+    file_list = os.listdir(f'./experiments/results/{time}/')
+    file_list = [file_name for file_name in file_list if f'random_experiment_dim_{n}' in file_name]
+
+    numbers = []
+    for file_name in file_list:
+        start_index = file_name.find('nr_') + 3
+        end_index = file_name.find('.json')
+        numbers.append(file_name[start_index:end_index])
+
+    for number in numbers:
+        experiment = Experiment(number)
+        experiment.read_custom_data('random_experiment_dim_3', folder=f'results/{time}')
+        experiment.run(5, save = False)
+        print(f'experiment number: {number}')
+        experiment.analyse_active_set_cycle()
 
 def try_custom_experiment(exp_nr, analyse=False):
     experiment = Experiment(exp_nr)
     experiment.read_custom_data('custom_experiment')
-    # experiment.QP.u = np.array([np.inf] * 3)
-    experiment.x_0 = np.array([-13,-9,0,0])
     experiment.run(5, save = False) #, method="newton")
     experiment.print()
     if analyse:
@@ -86,8 +84,8 @@ def try_custom_experiment(exp_nr, analyse=False):
 
 # try_multiplying_constant(30, 2)
 # try_different_bounds(17)
-        
-try_previous_random_experiment('2024-03-11_10-50-45')
-
 # try_random_experiment(100, n=2, upper_bound=True)
-# try_custom_experiment(30, analyse=True)
+        
+# try_previous_random_experiment('2024-03-11_11-20-58')
+        
+try_custom_experiment(25, analyse=True)
